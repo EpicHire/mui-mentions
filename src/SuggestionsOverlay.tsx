50d21e01c7f94a83fad7dddb36e70a1297351c20
Carson Brown <carwbrown@gmail.com>
2025-01-22 13:51:53 -0500

    adjust z-index

--- src/SuggestionsOverlay.tsx
+++ src/SuggestionsOverlay.tsx
@@ -218,7 +218,7 @@ function SuggestionsOverlay<T extends BaseSuggestionData>(props: SuggestionsOver
                 setFocusIndex={setFocusIndex}
                 setScrollFocusedIntoView={setScrollFocusedIntoView}
             />
-            <Popper open={true} anchorEl={cursorRef.current} placement='bottom-start' sx={{ zIndex: 2 }}>
+            <Popper open={true} anchorEl={cursorRef.current} placement='bottom-start' sx={{ zIndex: 1400 }}>
                 <Paper elevation={8} onMouseDown={onMouseDown}>
                     <List ref={ulElement} sx={{ width: '300px', maxHeight: '40vh', overflow: 'auto' }}>
                         {renderedSuggestions.length > 0
